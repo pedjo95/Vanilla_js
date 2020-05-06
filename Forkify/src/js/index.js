4599c4245085ff514c54cccb9c1a2980fc402484
pedjo95 <tec.ped95@gmail.com>
2020-05-06 19:58:32 -0300

    Add Shopping List functionality

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -1,8 +1,10 @@
 // http://forkify-api.herokuapp.com/
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /*
@@ -13,6 +15,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
  liked recipes
 */
 const state = {};
+window.state = state;
 
 
 /* Search Controller */
@@ -67,7 +70,6 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
   // get id from url
   const id = window.location.hash.replace('#', '');
-  console.log(id);
 
   if(id) {
     // prepare the UI for changes
@@ -102,6 +104,38 @@ const controlRecipe = async () => {
 // window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+
+/* List Controller*/
+const controlList = () => {
+  // create a new list
+  if(!state.list) state.list = new List();
+
+  // add a new ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  })
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  // delete event
+  if(e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // delete from state
+    state.list.deleteItem(id);
+
+    // delete form the UI
+    listView.deleteItem(id);
+
+    // count update
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // Handlig recipe button clicks
 elements.recipe.addEventListener('click', e => {
   if(e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -115,7 +149,13 @@ elements.recipe.addEventListener('click', e => {
     // increase clicked
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+
+  } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+
+    controlList();
   }
 
-  console.log(state.recipe);
 });
+
+
+window.l = new List();
