6c9500cbcbfeaa0fe49322367e4722b930c920b0
pedjo95 <tec.ped95@gmail.com>
2020-05-12 20:28:08 -0300

    Adding a POST request

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,10 +1,25 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts) {
+  // if(error) {
+  //   console.log(error);
+  // } else {
+  //   console.log(posts);
+  // }
+// });
+
+// Post Posts
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post"
+};
+// Create post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post) {
   if(error) {
     console.log(error);
   } else {
-    console.log(posts);
+    console.log(post);
   }
 });
+
--- HTTP/http.js
+++ HTTP/http.js
@@ -16,7 +16,18 @@ easyHTTP.prototype.get = function(url, callback) {
   }
   this.http.send();
 }
+
 // Make a HTTP POST request
-easyHTTP.prototype.post = function(url, data, callback)
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function() {
+    callback(null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
 // Make a HTTP PUT request
 // Make a HTTP DELETE request
