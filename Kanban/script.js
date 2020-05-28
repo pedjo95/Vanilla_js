34e0781e04f73fd2179f708274d4742d9b43c667
pedjo95 <tec.ped95@gmail.com>
2020-05-28 00:29:23 -0300

    A kanban-style application

--- Kanban/script.js
+++ Kanban/script.js
@@ -4,42 +4,45 @@ const dropzones = document.querySelectorAll('.dropzone');
 // Cards
 cards.forEach( card => {
   card.addEventListener('dragstart', dragStart);
-  card.addEventListener('drag', drag);
+  // card.addEventListener('drag', drag);
   card.addEventListener('dragend', dragEnd);
 })
 
 function dragStart() {
-  
-}
+  dropzones.forEach( dropzone => dropzone.classList.add('highlight'));
 
-function drag() {
-  
+  this.classList.add('is-dragging');
 }
 
 function dragEnd() {
-  
+  dropzones.forEach( dropzone => dropzone.classList.remove('highlight'));
+
+  this.classList.remove('is-dragging');
+
 }
 
 // Drops
 dropzones.forEach( dropzone => {
-  dropzone.addEventListener('dragenter', dragEnter);
+  // dropzone.addEventListener('dragenter', dragEnter);
   dropzone.addEventListener('dragover', dragOver);
   dropzone.addEventListener('dragleave', dragLeave);
   dropzone.addEventListener('drop', drop);
 })
 
-function dragEnter() {
-  console.log('Enter in dropzone');
-}
-
 function dragOver() {
-  console.log('In the dropzone');
+  this.classList.add('over');
+
+  // get the dragging card
+  const cardBeingDragged = document.querySelector('.is-dragging');
+
+  this.appendChild(cardBeingDragged);
 }
 
 function dragLeave() {
-  console.log('Left the dropzone');
+  this.classList.remove('over');
+
 }
 
 function drop() {
-  console.log('Dropped');
+  this.classList.remove('over');
 }
